38809d26cf47d2f4124fb4cbbda117893081e112
Nicola Sansom <nicolasansom1994@gmail.com>
2023-09-06 22:41:25 +0100

    Add comments and convert to arrow functions

--- src/util.tsx
+++ src/util.tsx
@@ -3,20 +3,34 @@ import { Cache } from "@raycast/api";
 const TIMER_CACHE_KEY = "timer";
 const cache = new Cache();
 
-let timer = null;
+type Timer = {
+  startedAt: number | null;
+  pausedAt: number | null;
+  elapsed: number;
+};
 
-function currentTimestamp(): number {
+const currentTimestamp = (): number => {
   return Math.floor(Date.now() / 1000);
-}
+};
 
-function startTimer() {
-  timer = { startedAt: currentTimestamp(), pausedAt: null, elapsed: 0 };
+/**
+ * Starts the timer and stores the timer object in cache.
+ */
+const startTimer = () => {
+  const timer: Timer = {
+    startedAt: currentTimestamp(),
+    pausedAt: null,
+    elapsed: 0,
+  };
 
   cache.set(TIMER_CACHE_KEY, JSON.stringify(timer));
-}
+};
 
-function pauseTimer() {
-  const timer = JSON.parse(cache.get(TIMER_CACHE_KEY) || "{}");
+/**
+ * Pauses the timer and updates the timer object in cache.
+ */
+const pauseTimer = () => {
+  const timer: Timer = JSON.parse(cache.get(TIMER_CACHE_KEY) || "{}");
 
   if (timer?.startedAt) {
     timer.pausedAt = currentTimestamp();
@@ -25,10 +39,13 @@ function pauseTimer() {
 
     cache.set(TIMER_CACHE_KEY, JSON.stringify(timer));
   }
-}
+};
 
-function resumeTimer() {
-  const timer = JSON.parse(cache.get(TIMER_CACHE_KEY) || "{}");
+/**
+ * Resumes the timer and updates the timer object in cache.
+ */
+const resumeTimer = () => {
+  const timer: Timer = JSON.parse(cache.get(TIMER_CACHE_KEY) || "{}");
 
   if (timer?.pausedAt) {
     timer.startedAt = currentTimestamp();
@@ -36,19 +53,30 @@ function resumeTimer() {
 
     cache.set(TIMER_CACHE_KEY, JSON.stringify(timer));
   }
-}
+};
 
-function resetTimer() {
+/**
+ * Resets the timer by removing the timer object from cache.
+ */
+const resetTimer = () => {
   cache.remove(TIMER_CACHE_KEY);
-}
+};
 
-function isTimerPaused(): boolean {
-  const timer = JSON.parse(cache.get(TIMER_CACHE_KEY) || "{}");
+/**
+ * Checks if the timer is paused.
+ * @returns {boolean} - True if the timer is paused, false otherwise.
+ */
+const isTimerPaused = (): boolean => {
+  const timer: Timer = JSON.parse(cache.get(TIMER_CACHE_KEY) || "{}");
   return timer.pausedAt != null;
-}
+};
 
-function getTimerState(): number | undefined {
-  const timer = JSON.parse(cache.get(TIMER_CACHE_KEY) || "{}");
+/**
+ * Gets the current state of the timer.
+ * @returns {number | undefined} - The elapsed time in seconds if the timer is started or paused, undefined otherwise.
+ */
+const getTimerState = (): number | undefined => {
+  const timer: Timer = JSON.parse(cache.get(TIMER_CACHE_KEY) || "{}");
 
   if (timer?.startedAt) {
     const elapsed = currentTimestamp() - timer.startedAt + timer.elapsed;
@@ -57,6 +85,6 @@ function getTimerState(): number | undefined {
     return timer.elapsed;
   }
   return undefined;
-}
+};
 
 export { startTimer, getTimerState, pauseTimer, resumeTimer, resetTimer, isTimerPaused };
